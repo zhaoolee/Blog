091be48b1b7db7ce30da67b081c361e2d6834e8b
zhaoolee <zhaoolee@gmail.com>
2019-06-13 15:13:09 +0800

    基本完成

--- blog_html/server/server.js
+++ blog_html/server/server.js
@@ -2,14 +2,12 @@ const express = require("express");
 const expressStatic = require("express-static");
 const superagent = require('superagent');
 const bodyParser = require('body-parser')
-const history = require('connect-history-api-fallback');
 
 const fs = require("fs");
 
 const cors = require("cors");
 let server = express();
 
-server.use('/', history())
 
 server.use(cors());
 
@@ -27,15 +25,27 @@ server.post("/api/github/blog", async function(req, res){
 
 })
 
-
-server.get("/*", async function(req, res){
-  let index_html = await fs.readFileSync("../dist/index.html")
-  res.set('Content-Type', 'text/html');
+// 前端页面
+server.get(/^\/zhaoolee.*/ , async function(req, res){
+  // 如果能找到对应的文件就返回文件
+  console.log(req.path);
+  let file_name = req.path.split("/").pop();
+  console.log("file_name:", file_name);
+  let index_html = "";
+  // 如果找不到就返回html
+  try{
+    index_html = await fs.readFileSync("../dist/"+file_name);
+    res.set('Content-Type', 'text/html');
+  }
+  catch(e) {
+    console.log(e);
+    index_html = await fs.readFileSync("../dist/index.html")
+    res.set('Content-Type', 'text/html');
+  }
   res.send(index_html);
-})
+}
 
-
-
+)
 
 server.listen(9090, function(){
   console.log("服务端口 http://localhost:9090/")
